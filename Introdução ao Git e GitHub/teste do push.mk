b94ce59d6c0d9e84c05ab9cd9a11edc35dabd9f0
Felipe Debiazi <105017242+felipedebiazi@users.noreply.github.com>
2022-05-19 12:33:13 -0300

    Update and rename testado push.txt to teste do push.mk

---
+++ Introdução ao Git e GitHub/teste do push.mk
@@ -0,0 +1,9 @@
+# Arquivo Teste para Push 
+Aprendemos atraves de alguns comandos como criar, modificar e atualizar arquivos e pastas em um repositorio remoto (Git/GitHub)
+
+## Comandos utilizados:
+
+- git status
+- git add .
+- git commit -m "inserir uma descrição sobre o que foi alterado"
+- git push origin main
